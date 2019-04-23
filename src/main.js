a2eecd643a7411335f096fd53d4fd32509ae7213
lau <laura.javiera77@gmail.com>
2019-04-23 10:07:25 -0400

    html con imagenmas codigo main

--- src/main.js
+++ src/main.js
@@ -1,1 +1,13 @@
-/* Manejo del DOM */
+document.getElementById("chile").addEventListener("click",()=>{
+   //let nuevoChile=[];
+    const filtroChile = Object.entries (WORLDBANK);
+    //for(let i=0; i< filtroChile.length; i++){
+       let nuevoChile= filtroChile[3]
+    //}
+   
+    console.log(nuevoChile);
+
+//root.innerHTML+=nuevoChile;
+    
+
+})
