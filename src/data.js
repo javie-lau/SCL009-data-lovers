fbb2b023c7f2e0a4372c45075dfbf7551562e5b2
lau <laura.javiera77@gmail.com>
2019-04-24 12:53:22 -0400

    paseando por la data

--- src/data.js
+++ src/data.js
@@ -1,10 +1,16 @@
 /* Manejo de data */
 
 // esta es una función de ejemplo
-// puedes ver como agregamos la función a nuestro objeto global window
+//puedes ver como agregamos la función a nuestro objeto global window
 
-const example = () => {
-  return 'example';
-};
+//const example = () => {
+ //return 'example';
+//};
 
-window.example = example;
+//window.example = example;
+
+
+
+//const filterCountry = Object.entries(WORLDBANK);
+
+    //console.log(filterCountry);
--- src/main.js
+++ src/main.js
@@ -1,13 +1,28 @@
 document.getElementById("chile").addEventListener("click",()=>{
-   //let nuevoChile=[];
-    const filtroChile = Object.entries (WORLDBANK);
-    //for(let i=0; i< filtroChile.length; i++){
-       let nuevoChile= filtroChile[3]
-    //}
    
-    console.log(nuevoChile);
 
-//root.innerHTML+=nuevoChile;
     
+   const filterChile = Object.entries(WORLDBANK.CHL);
 
+    const filterCHL= (filterChile[0])[1];
+      for(i=0;i<filterCHL.length;i++){
+            const filterYes=filterCHL[i];
+                 const dataChile= filterYes.data;
+                      console.log(dataChile);
+                     
+                     /* for( j=0;j<dataChile.length; j++){
+                          let data= dataChile[j];
+                           console.log(data);  } */                      
+                      
+                      
+                      const indicatorName=  filterYes.indicatorName;
+                       console.log(indicatorName);   
+                      
+                    
+                     }
+     
+         
+       
+         
 })
+ // root.innerHTML+= indicatorChile.indicatorName;
