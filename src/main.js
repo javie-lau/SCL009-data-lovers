90e8d19adcf56d13272e53dd9c5b03799d82838b
lau <laura.javiera77@gmail.com>
2019-04-25 08:24:19 -0400

    for in

--- src/main.js
+++ src/main.js
@@ -5,24 +5,35 @@ document.getElementById("chile").addEventListener("click",()=>{
    const filterChile = Object.entries(WORLDBANK.CHL);
 
     const filterCHL= (filterChile[0])[1];
+
+    for(const index in filterCHL){
+         const filterYes= filterCHL[index];
+        // console.log(filterYes);
+         const dataChile= filterYes.data;
+         //console.log(dataChile);
+         for (const index in dataChile){
+             
+               const arrDatos= dataChile[index];
+              console.log(arrDatos) 
+         
+              //root.innerHTML+= arrDatos;
+         }
+    }
+    /*
       for(i=0;i<filterCHL.length;i++){
             const filterYes=filterCHL[i];
                  const dataChile= filterYes.data;
                       console.log(dataChile);
                      
-                     /* for( j=0;j<dataChile.length; j++){
-                          let data= dataChile[j];
-                           console.log(data);  } */                      
+                               
                       
-                      
+                     
                       const indicatorName=  filterYes.indicatorName;
                        console.log(indicatorName);   
-                      
-                    
-                     }
+                                         
+                     }*/
      
          
-       
          
 })
- // root.innerHTML+= indicatorChile.indicatorName;
+ // 
