6b44550e1012d611706f302e95e09d99a0bd406f
lau <laura.javiera77@gmail.com>
2019-05-06 08:58:31 -0400

    tabla funcionando y paises

--- src/data.js
+++ src/data.js
@@ -1,12 +1,28 @@
 
 
-const peru =WORLDBANK.PER.indicators;
-const brasil= WORLDBANK.BRA.indicators;
-const mexico= WORLDBANK.MEX.indicators;
 
-function filterData(code){
+function filterData(pais, code){//agregue pais como parametro de la función
 
-const filterChile = Object.entries(WORLDBANK.CHL);//convertir objeto en array
+
+
+let country_indicator = window.WORLDBANK.CHL;//variable que s etoma y va cambiando con swith
+switch (pais) // me permite dar mas indicaciones por cada caso. paises 
+{
+   
+       case "WORLDBANK.BRA": 
+       country_indicator = window.WORLDBANK.BRA;//si aprietan brasil toma este let
+       break;
+       case "WORLDBANK.MEX": 
+       country_indicator = window.WORLDBANK.MEX;//si aprietan mexico toma este let
+       break;
+       case "WORLDBANK.PER": 
+       country_indicator = window.WORLDBANK.PER;//si aprietan peru toma este let
+       break;       
+   default: 
+     country_indicator = window.WORLDBANK.CHL;// por defecto chile
+}
+
+const filterChile = Object.entries(country_indicator);//convertir objeto en array
 const filterCHL= (filterChile[0])[1];
 
 let arrTime = [];
@@ -20,17 +36,29 @@ for(const index in filterCHL){//recorriendo indicadores
               
                arrTime.push(filterIndicator[obj])
               
-               arrtimeData=arrTime[0];
-              
+              arrtimeData=arrTime[0];
+               
               
           }
      }  
 
 }
  return (arrtimeData)
-  
+ 
 }
 
-window.filterData
+window.filterData = filterData;
 
+function order(arr_data){
+     let sort_array = arr_data.sort(function(a, b){
+          var keyA = new Date(a.updated_at),
+          keyB = new Date(b.updated_at);
+          // Compare the 2 dates
+          if(keyA < keyB) return -1;
+          if(keyA > keyB) return 1;
+          return 0;
+     });
 
+     return sort_array;
+}
+ 
--- src/main.js
+++ src/main.js
@@ -2,22 +2,22 @@
  
 document.getElementById("paginaUno").style.display="block";//invisible
 document.getElementById("root").style.display="none";//visible
-let show;
 
+let show="";
 
-document.getElementById("chile").addEventListener("click",()=>{
+document.getElementById("selectCountry").addEventListener("change",()=>{// boton chile
      
      document.getElementById("paginaUno").style.display="none";//invisible
      document.getElementById("root").style.display="block";//visible
      
-     document.getElementById("datChile").addEventListener("change",() =>{
+
+     document.getElementById("datChile").addEventListener("change",() =>{//select de indicadores
           
-          show = filterData(document.getElementById("datChile").value);//le estoy enviando parametro a mi funcion
-          console.table(show)
+          show =window.filterData(document.getElementById("selectCountry").value,document.getElementById("datChile").value);//le estoy enviando parametro a mi funcion, ambos parametros pais y code
+          
+         // alert(JSON.stringify(show));
           let key = Object.keys(show)
-          console.log(key);
           let value= Object.values(show)
-          console.log(value);
           //variables de tabla
           let hilera="";
           let celda="";
@@ -33,23 +33,22 @@ document.getElementById("chile").addEventListener("click",()=>{
                // Crea un elemento <table> y un elemento <tbody>
                let tabla   = document.createElement("table");
                let tblBody = document.createElement("tbody");
-              
+               document.getElementById("prueba").innerHTML = " "// para dejar vacio body entre cada tabla
                // Crea las celdas
                for (let i = 0; i < key.length; i++) {
                    
                     keyOne=key[i];
                     valueOne=value[i];
-                    console.log(keyOne)
                     
-                 // Crea las hileras de la tabla
-              
+                    if(valueOne){
+   
                  
                 hilera= document.createElement("tr");
                 celda = document.createElement("td");
                 celda1= document.createElement("td")
              
                textoCelda = document.createTextNode("año"+keyOne );
-               textoCelda1 = document.createTextNode("data" +valueOne);
+               textoCelda1 = document.createTextNode(valueOne);
                
                 celda1.appendChild(textoCelda1);
                 celda.appendChild(textoCelda);
@@ -58,24 +57,27 @@ document.getElementById("chile").addEventListener("click",()=>{
             
 
                 tblBody.appendChild(hilera);
-
+                    }
                }
-                 // agrega la hilera al final de la tabla (al final del elemento tblbody)
-               
+                            
                 
-               // posiciona el <tbody> debajo del elemento <table>
                tabla.appendChild(tblBody);
-               // appends <table> into <body>
                body.appendChild(tabla);
-               // modifica el atributo "border" de la tabla y lo fija a "2";
                tabla.setAttribute("border", "2");
                    
-     
+             
          
      });
-        
+
+    // ordenar tabla de indicador
+    //TableDesc.addEventListener("click", ()=>{
+    //alert(show.sort());
+//})
+
+
+     
 });
-// Obtener la referencia del elemento body
+
 
 
     
